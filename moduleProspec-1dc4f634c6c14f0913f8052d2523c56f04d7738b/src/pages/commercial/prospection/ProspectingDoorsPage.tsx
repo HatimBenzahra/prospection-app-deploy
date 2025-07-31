af4c08857fcd5701b0b8bbf8f674f8deb1847cc4
HatimBenzahra <benzahrahatim2001@gmail.com>
2025-07-31 20:54:23 +0200

    Fix TypeScript error - comment unused import
    - Comment PYTHON_SERVER_URL import to fix build error
    - Will be used later to replace getAudioServerUrl functions
    - Quick fix to get build working

--- moduleProspec-1dc4f634c6c14f0913f8052d2523c56f04d7738b/src/pages/commercial/prospection/ProspectingDoorsPage.tsx
+++ moduleProspec-1dc4f634c6c14f0913f8052d2523c56f04d7738b/src/pages/commercial/prospection/ProspectingDoorsPage.tsx
@@ -30,7 +30,7 @@ import { useSocket } from '@/hooks/useSocket';
 import { useAudioStreaming } from '@/hooks/useAudioStreaming';
 import { useDeepgramTranscription } from '@/hooks/useDeepgramTranscription';
 import { transcriptionHistoryService } from '@/services/transcriptionHistory.service';
-import { PYTHON_SERVER_URL } from '@/config';
+// import { PYTHON_SERVER_URL } from '@/config'; // TODO: Use this instead of getAudioServerUrl
 import { Mic, MicOff } from 'lucide-react';
 
 
